7cf118e066e364293dba1cd244a945425e460dbd
SokolovG <griso2010@yandex.ru>
2025-01-12 00:30:18 +0200

    FIrst router, viewset

---
+++ events/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+
+from .views import
+
+router = DefaultRouter()
+router.register('events', EventsListView, basename='event')
+
+
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
