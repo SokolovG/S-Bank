b8a11885ae0f7977da87dd542587ba4224570def
SokolovG <griso2010@yandex.ru>
2025-01-22 22:05:14 +0200

    Frontend initialized.

--- backend/DevEvents/DevEvents/settings.py
+++ backend/DevEvents/DevEvents/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'events.apps.EventsConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'djangorestframework_camel_case.middleware.CamelCaseMiddleWare',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5000",
 ]
 
 ROOT_URLCONF = 'DevEvents.urls'
