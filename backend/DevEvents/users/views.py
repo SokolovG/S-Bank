2b6b8bd8c13bfe27bf9228f24a1114c2a6a1b76f
SokolovG <griso2010@yandex.ru>
2025-02-04 16:29:24 +0200

    Django deleted. Basic Litestar structure added.

--- backend/DevEvents/users/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from rest_framework import generics
-
-from .models import User
-
-
-class ProfileView(LoginRequiredMixin, generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = ''
