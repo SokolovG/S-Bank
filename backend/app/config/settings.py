ad3eb3822aeb342888f6c805493b2985c79d0370
SokolovG <griso2010@yandex.ru>
2025-02-04 19:34:06 +0200

    Docker and docker compose optimized.

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -1,22 +1,26 @@
+import os
+from dotenv import load_dotenv
+
 from pydantic_settings import BaseSettings
 
+load_dotenv()
 
 class Settings(BaseSettings):
-    SECRET_KEY: str
-    DB_HOST: str = "localhost"
-    DB_PORT: int = 5432
-    DB_USER: str = "postgres"
-    DB_PASS: str = "postgres"
-    DB_NAME: str = "dev_events"
+    ENVIRONMENT: str = 'local'
+    SECRET_KEY: str  # оставляем, так как важно
+    DB_HOST: str = os.getenv('DB_HOST', 'localhost')
+    DB_PORT: int = int(os.getenv('DB_PORT', 5432))
+    DB_USER: str = os.getenv('DB_USER', 'postgres')
+    DB_PASS: str = os.getenv('DB_PASSWORD', 'postgres')
+    DB_NAME: str = os.getenv('DB_NAME', 'dev_events')
 
     @property
     def database_url(self) -> str:
-        """Return url for database."""
-        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+        host = 'localhost' if self.ENVIRONMENT == 'local' else 'db'
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{host}:{self.DB_PORT}/{self.DB_NAME}'
 
     class Config:
         env_file = '.env'
         env_file_encoding = 'utf-8'
-
 
 settings = Settings()
