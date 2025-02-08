e4971c5248ec9bdf2771e64c65cb4b5b26f0377a
SokolovG <griso2010@yandex.ru>
2025-02-08 22:28:28 +0200

    Controller updated, event_repository added to __init__

--- backend/app/api/v1/events/controllers.py
+++ backend/app/api/v1/events/controllers.py
@@ -9,18 +9,22 @@ from litestar import get, Controller
 
 from backend.app.domain.schemas.events.event import EventRead
 
+
 class EventController(Controller):
     """Basic event controller.
 
     Contains dependencies with EventRepository
     """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.event_repo = self.dependencies['repositories']['event_repo']
+
     @get()
     async def get_events(self, repositories: dict) -> list[EventRead] :
-        event_repo = repositories['event_repo']
-        events = await event_repo.list()
+        events = await self.event_repo.list()
         return events
 
     @get('/{event_id:int}')
     async def get_event(self, repositories: dict, event_id: int) -> EventRead:
-        event_repo = repositories['event_repo']
-        event = await event_repo.get_one_or_none(id=event_id)
+        event = await self.event_repo.get_one_or_none(id=event_id)
