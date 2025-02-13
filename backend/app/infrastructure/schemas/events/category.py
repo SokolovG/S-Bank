618399e24926e0424874e97c0daf694c111c67b3
SokolovG <griso2010@yandex.ru>
2025-02-13 15:43:41 +0200

    Slug validation

--- backend/app/infrastructure/schemas/events/category.py
+++ backend/app/infrastructure/schemas/events/category.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from datetime import datetime
 
+from pydantic import field_validator, model_validator
+
 from backend.app.infrastructure.schemas.base import BaseModel
 from backend.app.infrastructure.schemas.types import BasicString, DescriptionField
 
@@ -17,7 +19,18 @@ class CategoryRead(CategoryBase):
 
 
 class CategoryCreate(CategoryBase):
-    pass
+    @field_validator('slug')
+    @classmethod
+    def validate_slug(cls, v):
+        v = v.lower()
+        return v
+
+    @model_validator(mode='after')
+    @classmethod
+    def validate_name_and_slug_different(cls, data):
+        if data.name.lower() == data.slug:
+            raise ValueError('Slug should not be identical to the name.')
+        return data
 
 
 class CategoryUpdate(CategoryBase):
