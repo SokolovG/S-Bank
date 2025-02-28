368aa47a09b9affebe5f503d3bcf1278b5f36692
SokolovG <griso2010@yandex.ru>
2025-02-28 10:40:17 +0200

    Small type fixes

--- backend/app/api/v1/events/controllers.py
+++ backend/app/api/v1/events/controllers.py
@@ -9,18 +9,22 @@ from backend.app.infrastructure.schemas.events.event import EventRead
 class EventController(Controller):
     """Basic event controller."""
 
-    path = '/events'
+    path = "/events"
 
     @get()
-    async def get_all_events(self, repositories: RepositoryDependencies) -> list[EventRead]:
+    async def get_all_events(
+        self, repositories: RepositoryDependencies
+    ) -> list[EventRead]:
         """Get all events."""
-        event_repo = repositories.get('event_repo')  # Get repo.
+        event_repo = repositories.get("event_repo")  # Get repo.
         events = await event_repo.list()
         return [EventRead.model_validate(event) for event in events]
 
-    @get('/{event_id:int}')
-    async def get_event_by_pk(self, repositories: RepositoryDependencies, event_id: int) -> EventRead:
+    @get("/{event_id:int}")
+    async def get_event_by_pk(
+        self, repositories: RepositoryDependencies, event_id: int
+    ) -> EventRead:
         """Get event by_pk."""
-        event_repo = repositories.get('event_repo')  # Get repo.
+        event_repo = repositories.get("event_repo")  # Get repo.
         event = await event_repo.get_one_or_none(id=event_id)
         return EventRead.model_validate(event)
